de122317c6a16f602415e0b43bbb81ddae2b776c
SARAGADAM VAMSI VARSHITH <101577954+vamsi9385@users.noreply.github.com>
2024-03-03 17:39:04 +0530

    Update weatherforcaste.js

--- weatherforcaste.js
+++ weatherforcaste.js
@@ -9,16 +9,14 @@ const apikey="9825fdc91b4bd98c72e4af4a865d312a"
 async function checkweather(city){
     const response=await fetch(apiUrl+ city +`&appid=${apikey}`)
     var data=await response.json();
-
-    console.log(data)
-    document.querySelector(".weather__temperature").innerHTML=data.main.temp
+    document.querySelector(".weather__temperature").innerHTML=Math.ceil(data.main.temp)
     document.querySelector(".weather__forecast").innerHTML=city
-    document.querySelector(".Min ").innerHTML="min : "+data.main.temp_min
-    document.querySelector(".Max ").innerHTML="max : "+data.main.temp_max
-    document.querySelector(".weather__realfeel ").innerHTML=data.main.feels_like
-    document.querySelector(".weather__humidity").innerHTML=data.main.humidity
-    document.querySelector(".weather__wind").innerHTML=data.wind.speed
-    document.querySelector(".weather__pressure").innerHTML=data.main.pressure
+    document.querySelector(".Min ").innerHTML="min : "+Math.ceil(data.main.temp_min)
+    document.querySelector(".Max ").innerHTML="max : "+Math.ceil(data.main.temp_max)
+    document.querySelector(".weather__realfeel ").innerHTML=Math.ceil(data.main.feels_like)
+    document.querySelector(".weather__humidity").innerHTML=Math.ceil(data.main.humidity)
+    document.querySelector(".weather__wind").innerHTML=Math.ceil(data.wind.speed)
+    document.querySelector(".weather__pressure").innerHTML=Math.ceil(data.main.pressure)
 
 }
 
